8b6c611dea86e278caa3c929ec22040c40bc274f
QuangVuong85 <quangvuong0805@gmail.com>
2019-11-05 01:53:43 +0700

    delete folder utils

--- src/main/java/sbcrudrestful/controller/MainRESTController.java
+++ src/main/java/sbcrudrestful/controller/MainRESTController.java
@@ -10,81 +10,9 @@ import java.util.List;
 
 @RestController
 public class MainRESTController {
-
-   /* @Autowired
-    private EmployeeDAO employeeDAO;*/
-
     @RequestMapping("/")
     @ResponseBody
     public String welcome() {
         return "Welcome to RestfulAPI Example.";
     }
-
-/*    // URL:
-    // http://localhost:8080/employees
-    // http://localhost:8080/employees.xml
-    // http://localhost:8080/employees.json
-    @RequestMapping(value = "/employees", //
-            method = RequestMethod.GET, //
-            produces = { MediaType.APPLICATION_JSON_VALUE, //
-                    MediaType.APPLICATION_XML_VALUE })
-    @ResponseBody
-    public List<Employee> getEmployees() {
-        List<Employee> list = employeeDAO.getAllEmployees();
-        return list;
-    }
-
-    // URL:
-    // http://localhost:8080/employee/{empNo}
-    // http://localhost:8080/employee/{empNo}.xml
-    // http://localhost:8080/employee/{empNo}.json
-    @RequestMapping(value = "/employee/{empNo}", //
-            method = RequestMethod.GET, //
-            produces = { MediaType.APPLICATION_JSON_VALUE, //
-                    MediaType.APPLICATION_XML_VALUE })
-    @ResponseBody
-    public Employee getEmployee(@PathVariable("empNo") String empNo) {
-        return employeeDAO.getEmployee(empNo);
-    }
-
-    // URL:
-    // http://localhost:8080/employee
-    // http://localhost:8080/employee.xml
-    // http://localhost:8080/employee.json
-
-    @RequestMapping(value = "/employee", //
-            method = RequestMethod.POST, //
-            produces = { MediaType.APPLICATION_JSON_VALUE, //
-                    MediaType.APPLICATION_XML_VALUE })
-    @ResponseBody
-    public Employee addEmployee(@RequestBody Employee emp) {
-        System.out.println("(Service Side) Creating employee: " + emp.getEmpNo());
-        return employeeDAO.addEmployee(emp);
-    }
-
-    // URL:
-    // http://localhost:8080/employee
-    // http://localhost:8080/employee.xml
-    // http://localhost:8080/employee.json
-    @RequestMapping(value = "/employee", //
-            method = RequestMethod.PUT, //
-            produces = { MediaType.APPLICATION_JSON_VALUE, //
-                    MediaType.APPLICATION_XML_VALUE })
-    @ResponseBody
-    public Employee updateEmployee(@RequestBody Employee emp) {
-        System.out.println("(Service Side) Editing employee: " + emp.getEmpNo());
-        return employeeDAO.updateEmployee(emp);
-    }
-
-    // URL:
-    // http://localhost:8080/employee/{empNo}
-    @RequestMapping(value = "/employee/{empNo}", //
-            method = RequestMethod.DELETE, //
-            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
-    @ResponseBody
-    public void deleteEmployee(@PathVariable("empNo") String empNo) {
-        System.out.println("(Service Side) Deleting employee: " + empNo);
-        employeeDAO.deleteEmployee(empNo);
-    }*/
-
 }
